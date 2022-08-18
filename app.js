d4e54e61bc3500747fdf43190eb245bced33f3d7
Rajesh Tiwari <71480531+rajesht738@users.noreply.github.com>
2022-08-19 00:02:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4848;
 const postRouter = require('./routes/postRouter');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({origin: 'http://localhost:3000'}))
+app.use(cors({origin: true}))
 
 app.use('/api/post', postRouter);
 // we have to put it after router call for globle Error message
